7c8e6bff8505d71de4640a5f7fc4342b6c99ad94
kb <86767129+kbdevs@users.noreply.github.com>
2024-01-22 10:57:38 -0800

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -22,7 +22,7 @@ try:
 
     localv_path = "localv.json"
 
-    if not os.path.exists(localv_path):
+    if not os.path.exists(localv_path) or not os.path.exists(file_paths[1]):
         local_version = "0.0.0"
         data = {
             "version": remote_version,
