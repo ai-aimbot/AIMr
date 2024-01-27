e74497a841fbf208dfd53acde1f568a6b388e8ba
kb <86767129+kbdevs@users.noreply.github.com>
2024-01-26 21:43:06 -0800

    Update AIMr.py

--- AIMr.py
+++ AIMr.py
@@ -1,9 +1,11 @@
 import os
-import urllib.request
-import zipfile
 import json
 import shutil
+import zipfile
 import subprocess
+import urllib.request
+
+
 
 try:
     newest_version = "https://raw.githubusercontent.com/kbdevs/ai-aimbot/main/current_version.txt"
@@ -27,7 +29,8 @@ try:
         local_version = "0.0.0"
         data = {
             "version": remote_version,
-            "pip": False
+            "pip": False,
+            "python": False,
         }
         with open(localv_path, "w") as file:
             json.dump(data, file)
@@ -37,57 +40,82 @@ try:
             local_version = data["version"]
 
     if remote_version != local_version:
-        option = True if input("Your version of AIMr is out of date, would you like to update? (y/n): ").lower() == "y" else False
-        if option:
-            for file_path in file_paths:
-                if os.path.exists(file_path):
-                    try:
-                        os.remove(file_path)
-                    except Exception as e:
-                        print(f"Error occurred while removing {file_path}: {e}")
 
-            print("Downloading...")
-            # Download the zip file
-            url = "https://codeload.github.com/kbdevs/ai-aimbot/zip/refs/heads/main"
-            response = urllib.request.urlopen(url)
-            zip_content = response.read()
+        print("Deleting old files...")
+        for file_path in file_paths:
+            if os.path.exists(file_path):
+                try:
+                    os.remove(file_path)
+                except Exception as e:
+                    print(f"Error occurred while removing {file_path}: {e}")
 
-            # Save the zip file
-            with open("ai-aimbot.zip", "wb") as file:
-                file.write(zip_content)
+        print("Downloading AIMr...")
+        # Download the zip file
+        url = "https://codeload.github.com/kbdevs/ai-aimbot/zip/refs/heads/main"
+        response = urllib.request.urlopen(url)
+        zip_content = response.read()
 
-            print("Unzipping...")
-            # Unzip the file
-            with zipfile.ZipFile("ai-aimbot.zip", "r") as zip_ref:
-                zip_ref.extractall("ai-aimbot")
-            os.remove("ai-aimbot.zip")
+        # Save the zip file
+        with open("ai-aimbot.zip", "wb") as file:
+            file.write(zip_content)
 
-            print("Moving files...")
-            # Move files from ai-aimbot/ to current directory
-            for root, dirs, files in os.walk("ai-aimbot"):
-                for file in files:
-                    shutil.move(os.path.join(root, file), os.path.join(".", file))
+        print("Unzipping...")
+        # Unzip the file
+        with zipfile.ZipFile("ai-aimbot.zip", "r") as zip_ref:
+            zip_ref.extractall("ai-aimbot")
+        os.remove("ai-aimbot.zip")
 
-            # Remove ai-aimbot-testing/ directory
-            shutil.rmtree("ai-aimbot")
+        print("Moving files...")
+        # Move files from ai-aimbot/ to current directory
+        for root, dirs, files in os.walk("ai-aimbot"):
+            for file in files:
+                shutil.move(os.path.join(root, file), os.path.join(".", file))
+
+        # Remove ai-aimbot-testing/ directory
+        shutil.rmtree("ai-aimbot")
+
+        with open("localv.json", "w") as file:
+            data["version"] = remote_version
+            json.dump(data, file)
+
+        with open("localv.json", "r") as file:
+            pls = json.load(file)
+            python = pls["python"]
+
+        if python is not True:
+            print("Downloading python...")
+            # Download the python
+            url = "https://www.python.org/ftp/python/3.12.1/python-3.12.1-amd64.exe"
+            filename = "pythoninstaller.exe"
+            urllib.request.urlretrieve(url, filename)
+
+            print("Installing python...")
+            subprocess.run([filename, "/quiet", "InstallAllUsers=1", "PrependPath=1", "Include_test=0"])
 
             with open("localv.json", "w") as file:
-                data["version"] = remote_version
-                json.dump(data, file)
+                pls["python"] = True
+                json.dump(pls, file)
 
-            with open("localv.json", "r") as file:
-                data2 = json.load(file)
-                pip = data["pip"]
+            os.remove(filename)
 
-            if pip is not True:
-                print("Installing required modules...")
-                os.system("pip install -r req.txt")
-                os.system("pip3 install -r req.txt")
-                with open("localv.json", "w") as file:
-                    data2["pip"] = True
-                    json.dump(data2, file)
 
-    subprocess.run(["python", "library.py"])
+        with open("localv.json", "r") as file:
+            data2 = json.load(file)
+            pip = data["pip"]
+
+        if pip is not True:
+            print("Installing required modules...")
+            os.system("pip install -r req.txt")
+            os.system("pip3 install -r req.txt")
+            with open("localv.json", "w") as file:
+                data2["pip"] = True
+                json.dump(data2, file)
+
+    if os.path.exists("library.py"):
+        subprocess.run(["python", "library.py"])
+    else:
+        print("Failed to update, please delete localv.json and launch this again.")
+        exit()
 
 except KeyboardInterrupt:
     exit()
